f84ead27b25d8d5759b275eb8895d07b0363c2d2
vijaycode12 <ajayreddy6601@gmail.com>
2025-01-10 00:17:40 +0530

    main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from pyspark.sql import SparkSession
 from config.config import configuration  # Assuming configuration is a module where you retrieve AWS credentials
+from pyspark.sql.types import StructType, StructField, StringType, DoubleType, DateType
 
 if __name__=="__main__":
 # Set up the Spark session
@@ -11,5 +12,33 @@ if __name__=="__main__":
         .config('spark.hadoop.fs.s3a.secret.key', configuration.get('AWS_SECRET_KEY')) \
         .config('spark.hadoop.fs.s3a.aws.credentials.provider', 'org.apache.hadoop.fs.s3a.SimpleAWSCredentialsProvider') \
         .getOrCreate()
+        
+    text_input_dir='input\input_text'
+    json_input_dir='input\input_json'
+    csv_input_dir='input\input_csv'
+    pdf_input_dir='input\input_pdf'
+    video_input_dir='input\input_video'
+    img_input_dir='input\input_img'
+
 
 # Now you can use the spark session to access data from AWS S3
+
+    data_schema = StructType([
+        StructField("file_name", StringType(), True),
+        StructField("position", StringType(), True),
+        StructField("classcode", StringType(), True),
+        StructField("salary_start", DoubleType(), True),
+        StructField("salary_end", DoubleType(), True),
+        StructField("start_date", DateType(), True),
+        StructField("end_date", DateType(), True),
+        StructField("req", StringType(), True),
+        StructField("notes", StringType(), True),
+        StructField("duties", StringType(), True),
+        StructField("selection", StringType(), True),
+        StructField("experience_length", StringType(), True),
+        StructField("job_type", StringType(), True),
+        StructField("education_length", StringType(), True),
+        StructField("school_type", StringType(), True),
+        StructField("application_location", StringType(), True),
+        
+    ])
